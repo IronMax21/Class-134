a9e7bda1a285fb7bc75e9d6ea52a1b524d980051
IronMax21 <91037892+IronMax21@users.noreply.github.com>
2023-03-12 20:34:03 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,63 @@
+img = "";
+status = "";
+objects = [];
+
+//called before loading
+function preload()
+{
+    img = loadImage('office.jpg');
+}
+//used to create canvas
+function setup()
+{
+    canvas = createCanvas(380, 380);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(380, 380);
+    video.hide();
+}
+function start()
+{
+    //initialize cocossd and store cocossd into object detector variable and run modelLoaded function
+    objectDetector = ml5.objectDetector("cocossd", modelLoaded);
+    document.getElementById("status").innerHTML = "status  =  detecting objects";
+}
+function modelLoaded()
+{
+    console.log("model is loaded");
+    status = true;
+    objectDetector.detect(video, gotResult);
+}
+function gotResult(error, results)
+{
+    if(error)
+    {
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
+}
+//executed in every frame
+function draw()
+{
+    image(video, 0, 0, 380, 380);
+
+    if(status != "")
+    {
+        r = random(255);
+        g = random(255);
+        b = random(255);
+        objectDetector.detect(video, gotResult);
+        for(i = 0; i < objects.length; i++)
+        {
+        document.getElementById("status").innerHTML = "Status : object detected ";
+        document.getElementById("number_of_objects").innerHTML = "number of objects detected are - "+objects.length
+        fill(r, g, b);
+        percent = floor(objects[i].confidence * 100);
+        text(objects[i].label + " "+percent+"%", objects[i].x + 20, objects[i].y+ 20);
+        noFill();
+        stroke(r, g, b);
+        rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
+}
